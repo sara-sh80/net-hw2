30e3e0de98f021d9a269267b3f88abe053e2d642
sara-sh80 <60575647+sara-sh80@users.noreply.github.com>
2023-11-16 22:02:03 +0330

    adding local storage

--- src/checkList.js
+++ src/checkList.js
@@ -1,9 +1,33 @@
-var arr = []
+// Save array to local storage
+function saveArrayToLocalStorage() {
+    console.log("saving")
+    myarr = []
+    for (i = 0; i < arr.length; i++){
+        myarr.push({
+            "id": arr[i].id,
+            "title": arr[i].title,
+            "completed": arr[i].checked
+        })
+    }
+    console.log(myarr)
+    localStorage.setItem("tasks", JSON.stringify(myarr))
+}
 
-var AddToList = () =>{
+// Retrieve array from local storage
+function getArrayFromLocalStorage() {
+    console.log("loading")
+    const array = localStorage.getItem("tasks")
+    console.log(array)
+    myarr = array ? JSON.parse(array) : []
+    for (i = 0; i < myarr.length; i++){
+        arr.push(addTaskToDemo(
+            myarr[i].id, myarr[i].title, myarr[i].completed
+        ))
+    }
+}
 
-    //create label element
-    var name = document.getElementById("inp1").value
+
+function addTaskToDemo(id, name, completed) {
     var label = document.createElement("label")
     label.innerText = name
     label.id = name
@@ -11,11 +35,17 @@ var AddToList = () =>{
 
     //create input element
     var input = document.createElement("input")
-    arr.push(input)
-    console.log(arr)
+
     input.id = arr.length
     input.setAttribute('type', 'checkbox')
     input.name = name
+    input.addEventListener('change', (event) => {
+      if (event.currentTarget.checked) {
+        alert('checked');
+      } else {
+        alert('not checked');
+      }
+    })
 
     //create title attribute for task
     const titleAtt = document.createAttribute("title")
@@ -28,7 +58,22 @@ var AddToList = () =>{
     input.setAttributeNode(completedAtt)
 
     document.getElementById(name).appendChild(input)
+    return input
+}
 
+
+var arr = []
+getArrayFromLocalStorage()
+
+var AddToList = () =>{
+
+    //create label element
+    var name = document.getElementById("inp1").value
+    document.getElementById("inp1").value = ""
+
+    input = addTaskToDemo(arr.length, name, false)
+    arr.push(input)
+    saveArrayToLocalStorage()
 
     var count = document.getElementById("c3").innerText
     count = Number(count)
@@ -40,6 +85,7 @@ var AddToList = () =>{
     count = count + 1
     document.getElementById("c1").innerHTML = count
 
+    saveArrayToLocalStorage(arr)
 
     // for (i = 0; i < arr.length; i++){
     //     // console.log(arr.length)
